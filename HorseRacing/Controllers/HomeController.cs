7dcfd96165795ac7e3889447fc01cce32e5e660c
Jovana Mitic <jovanamitic@protonmail.com>
2022-04-04 11:46:34 +0100

    Connect to the database with EF and create a CreateHorse method in Home Controller. Add a view for CreateHorse.

--- HorseRacing/Controllers/HomeController.cs
+++ HorseRacing/Controllers/HomeController.cs
@@ -8,9 +8,36 @@ namespace HorseRacing.Controllers
 {
     public class HomeController : Controller
     {
+        HorseRacingAppDBEntities db = new HorseRacingAppDBEntities();
         public ActionResult Index()
         {
             return View();
+        }
+
+        // Create a horse - get data
+        public ActionResult CreateHorse()
+        {
+            Horse horse = new Horse();
+            return View();
+        }
+
+        // Create a horse - post data
+        [HttpPost]
+        public ActionResult CreateHorse(Horse h)
+        {
+            Horse horse = new Horse();
+            horse.Name = h.Name;
+            horse.DateOfBirth = h.DateOfBirth;
+            horse.DamId = h.DamId;
+            horse.SireId = h.SireId;
+            horse.ColourId = h.ColourId;
+            horse.CategoryId = h.CategoryId;
+            horse.GenderId = h.GenderId;
+            horse.CountryId = h.CountryId;
+            horse.AcqusitionId = h.AcqusitionId;
+            db.Horses.Add(horse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
